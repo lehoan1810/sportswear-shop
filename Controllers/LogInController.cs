2057fcd066cc71d1e4bf56dec383ff88ecf57cc5
Le Hoan <hoanhao18102000@gmail.com>
2020-10-28 22:46:46 +0700

    Initial commit

---
+++ Controllers/LogInController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace database.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult login()
+        {
+            return View();
+        }      
+        
+    }
+}
